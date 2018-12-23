edb470e18a392c88e3b41a3344ee95eb649d6c2b
Stephanie Coates <stcoates8@gmail.com>
2018-12-22 19:36:03 -0700

    authentication section complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
-import routes from './routes/routes';
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import "./App.css";
+import routes from "./routes/routes";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import {withRouter} from 'react-router-dom'
 
 class App extends Component {
+  async componentDidMount() {
+    let res = await axios.get('/auth/sessiondata');
+    console.log("initial cdm", res.data);
 
-async componentDidMount () {
-let res = await Axios.get('/api/sessiondata')
-console.log('initial cdm', res)
-}
+  }
+
+  async logout () {
+    await axios.post('/auth/logout');
+    this.props.history.push('/login')
+  }
 
   render() {
     return (
       <div className="App">
-        <Link to='/login'><button>Login page</button></Link>
-        <Link to='/protocol'><button>Protocol</button></Link>
-        <Link to='/dashboard'><button>dashboard</button></Link>
-        <Link to='/cancelemergency'><button>cancel Emergency</button></Link>
-        
-        
+        <Link to="/login">
+          <button>Login page</button>
+        </Link>
+        <Link to="/protocol">
+          <button>Protocol</button>
+        </Link>
+        <Link to="/dashboard">
+          <button>dashboard</button>
+        </Link>
+        <Link to="/cancelemergency">
+          <button>cancel Emergency</button>
+        </Link>
+      <button onClick={() => this.logout()}>Logout</button>
         {routes}
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/components/Auth/AdminSignup.js
+++ src/components/Auth/AdminSignup.js
@@ -1,69 +1,113 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import { connect } from 'react-redux'
-import { updateSchool } from '../../dux/reducer';
-import { updateAdmin } from '../../dux/reducer';
-
-const emptyField = 'Please enter email and password.'
-
+import React, { Component } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import { updateSchool, updateAdmin } from "../../dux/reducer";
+import InputMask from "react-input-mask";
 
 class AdminSignup extends Component {
   state = {
-    schoolName: '',
-    schoolCity: '',
-    schoolState: '',
-    adminFirst: '',
-    adminLast: '',
-    adminPhone: '',
-    adminEmail: '',
-    adminPassword: ''
-  }
-  
+    schoolName: "",
+    schoolCity: "",
+    schoolState: "",
+    adminFirst: "",
+    adminLast: "",
+    adminPhone: "",
+    adminEmail: "",
+    adminPassword: "",
+    errMsg: ""
+  };
+
   componentDidMount() {
     this.firstInput.focus();
   }
 
-  async signup() {
-    try {
-      if (!this.state.schoolName ||
-        !this.state.schoolCity ||
-        !this.state.schoolState ||
-        !this.state.adminFirst ||
-        !this.state.adminLast ||
-        !this.state.adminPhone ||
-        !this.state.adminEmail ||
-        !this.state.adminPassword
-          ) return alert(emptyField)
-      await axios.post('/auth/signup', {
-        schoolName: this.state.schoolName,
-        schoolCity: this.state.schoolCity,
-        schoolState: this.state.schoolState,
-        adminFirst: this.state.adminFirst,
-        adminLast: this.state.adminLast,
-        adminPhone: this.state.adminPhone,
-        adminEmail: this.state.adminEmail,
-        adminPassword: this.state.adminPassword
-      })
+  noEmptyInputsValidator() {
+    if (
+      !this.state.schoolName ||
+      !this.state.schoolCity ||
+      !this.state.schoolState ||
+      !this.state.adminFirst ||
+      !this.state.adminLast ||
+      !this.state.adminPhone ||
+      !this.state.adminEmail ||
+      !this.state.adminPassword
+    ) {
+      return null;
+    } else {
+      return true;
     }
-    catch (error) {
-      // alert(error.response.request.response)
-      console.log("This is broken")
+  }
+
+  phoneNumberValidator(phoneNum) {
+    if (phoneNum) {
+      var formatted = phoneNum.replace(/\D/g, "");
+      if (formatted.length !== 11) {
+        this.setState({
+          errMsg: "Please enter in a valid phone number."
+        });
+        return null;
+      } else {
+        return `+${formatted}`;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  emailValidator(email) {
+    if (email.includes("@") && email.includes(".")) {
+      return true;
+    } else {
+      this.setState({
+        errMsg: "Please enter a valid email address."
+      });
+      return null;
+    }}
+
+  async signup() {
+    let filledInputsValidated = this.noEmptyInputsValidator();
+    let validPhoneNumber = this.phoneNumberValidator(this.state.adminPhone);
+    let validEmail = this.emailValidator(this.state.adminEmail);
+    if (filledInputsValidated && validPhoneNumber && validEmail) {
+      try {
+        let res = await axios.post("/auth/signup", {
+          schoolName: this.state.schoolName,
+          schoolCity: this.state.schoolCity,
+          schoolState: this.state.schoolState,
+          adminFirst: this.state.adminFirst,
+          adminLast: this.state.adminLast,
+          adminPhone: validPhoneNumber,
+          adminEmail: this.state.adminEmail,
+          adminPassword: this.state.adminPassword
+        });
+        this.props.updateAdmin(res.data.admin);
+        this.props.history.push('/dashboard')
+      } catch (err) {
+        console.log(err);
+        this.setState({
+          errMsg:
+            "An account under that email already exists. Please log in instead."
+        });
+      }
+    } else if (!filledInputsValidated) {
+      this.setState({
+        errMsg: "Please fill in all fields above."
+      });
     }
   }
 
   handleInputChange = name => event => {
     this.setState({
       [name]: event.target.value,
+      errMsg: ""
     });
   };
 
   handleKeyPress(event) {
-    if (event === 'Enter') {
+    if (event === "Enter") {
       this.signup();
     }
   }
-
-
 
   render() {
     return (
@@ -74,62 +118,62 @@ class AdminSignup extends Component {
             type="text"
             placeholder="School Name"
             value={this.state.schoolName}
-            onChange={this.handleInputChange('schoolName')}
-            ref={(input) => { this.firstInput = input; }}
+            onChange={this.handleInputChange("schoolName")}
+            ref={input => {
+              this.firstInput = input;
+            }}
           />
           <input
             type="text"
             placeholder="School City"
             value={this.state.schoolCity}
-            onChange={this.handleInputChange('schoolCity')}
+            onChange={this.handleInputChange("schoolCity")}
           />
           <input
             type="text"
             placeholder="School State"
             value={this.state.schoolState}
-            onChange={this.handleInputChange('schoolState')}
+            onChange={this.handleInputChange("schoolState")}
           />
           <input
             type="text"
             placeholder="First Name"
             value={this.state.adminFirst}
-            onChange={this.handleInputChange('adminFirst')}
+            onChange={this.handleInputChange("adminFirst")}
           />
           <input
             type="text"
             placeholder="Last Name"
             value={this.state.adminLast}
-            onChange={this.handleInputChange('adminLast')}
+            onChange={this.handleInputChange("adminLast")}
           />
-          <input
-            type="number"
+          <InputMask
+            mask="+1 (999) 999-9999"
+            maskChar={null}
             placeholder="Phone Number"
-            value={this.state.adminPhone}
-            onChange={this.handleInputChange('adminPhone')}
+            onChange={this.handleInputChange("adminPhone")}
           />
           <input
             type="text"
             placeholder="Email"
             value={this.state.adminEmail}
-            onChange={this.handleInputChange('adminEmail')}
+            onChange={this.handleInputChange("adminEmail")}
           />
           <input
             type="text"
             placeholder="Password"
             value={this.state.adminPassword}
-            onChange={this.handleInputChange('adminPassword')}
-            onKeyPress={(e) => this.handleKeyPress(e.key)}
+            onChange={this.handleInputChange("adminPassword")}
+            onKeyPress={e => this.handleKeyPress(e.key)}
           />
         </form>
-      </div >
+        <p style={{ color: "red", fontSize: "0.7em" }}>{this.state.errMsg}</p>
+        <button onClick={() => this.signup()}>Signup</button>
+      </div>
     );
   }
 }
-// function mapStateToProps(state){
-//   return{
-//     admin: state.admin,
-//     school: state.school
-//   }
-// }
-let mapDispatchToProps = { updateSchool, updateAdmin }
+
+let mapDispatchToProps = { updateSchool, updateAdmin };
+
 export default connect(null, mapDispatchToProps)(AdminSignup);
