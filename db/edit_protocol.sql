33b9f0a48689dc8a0e92635312492a61722583dd
Stephanie Coates <stcoates8@gmail.com>
2019-01-01 20:09:21 -0700

    setup active shooter protocol page, admin can read and edit

---
+++ db/edit_protocol.sql
@@ -0,0 +1,16 @@
+UPDATE protocol
+SET 
+protocol_1 = $1,
+protocol_2 = $2,
+protocol_3 = $3,
+protocol_4 = $4,
+protocol_5 = $5,
+protocol_6 = $6,
+protocol_7 = $7,
+protocol_8 = $8,
+protocol_9 = $9,
+protocol_10 = $10
+
+
+WHERE protocol_name = $11 and school_id = $12
+returning *;
