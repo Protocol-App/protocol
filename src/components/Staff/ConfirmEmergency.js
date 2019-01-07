da4d98f9eed14217ed0e4cf42bdeb2316423f312
dnljsn <daniel@kalood.com>
2019-01-07 11:06:08 -0700

    Swipe to unlock in progress

--- src/components/Staff/ConfirmEmergency.js
+++ src/components/Staff/ConfirmEmergency.js
@@ -28,17 +28,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import openSocket from 'socket.io-client';
 import axios from 'axios';
+import icon from '../../assets/progress-icons/progress-icon-2.png';
+import BackArrow from '../../assets/back-arrow.svg';
+import Toggle from "react-switch";
 const socket = openSocket('http://localhost:4000/');
 
 class ConfirmEmergency extends Component {
   constructor() {
     super();
 
-    this.state = {
-
-    }
+    this.state = { checked: false };
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  handleChange(checked) {
+    this.setState({ checked });
+  }
 
   countdown() {
     //settimeout to count down 5 seconds, display countdown on screen
@@ -53,14 +58,65 @@ class ConfirmEmergency extends Component {
     let res = await axios.post('/api/confirmemergency', { emergencyName, userID, schoolID, swiped })
     socket.emit('emergency', res.data)
   }
-  render() {
-    return (
-      <div>
-          Confirm Emergency
-        <button onClick={() => this.sendEmergency()}>Emit</button>
-      </div>
-    );
+
+  unlockSwipe() {
+    var theRange = this.value;
+    if (theRange == 100) {
+
+      console.log('its working!');
+    }
   }
+//     } else {
+//       document.init = setInterval(function () {
+//         if (document.querySelector("input[type=\"range\"]").value != 0) {
+//           document.querySelector("input[type=\"range\"]").value = theRange--;
+//         }
+//       }, 1);
+//     }
+//   }
+
+//   document.querySelector("input[type=\"range\"]").onmousedown = function () {
+//     clearInterval(document.init);
+//   }
+
+// function unlock() {
+//   document.querySelector("input[type=\"range\"]").style.opacity = "0";
+// }
+
+render() {
+  return (
+    <div
+      className='dark-background'>
+      <img className='logo' src={icon} alt="Protocol Logo" />
+      <input
+        type="range"
+        class="slideToUnlock"
+        // value="0"
+        max="100"
+        // onMouseUp={()=> this.unlockSwipe}
+      />
+      {/* <label htmlFor="toggle-switch">
+            <Toggle
+              className='toggle'
+              checked={this.state.checked}
+              onChange={this.handleChange}
+              onColor="#0088ff"
+              offColor="#0088ff"
+              onHandleColor="#ffffff"
+              handleDiameter={50}
+              uncheckedIcon={false}
+              checkedIcon={false}
+              // boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+              // activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+              height={24}
+              width={200}
+            id="toggle-switch"
+            />
+          </label> */}
+      <button onClick={() => this.sendEmergency()}>Emit</button>
+    </div>
+  );
+}
 }
 
 function mapStateToProps(state) {
